6e51cfebe3c238c27fc8efe4658726d2bcbd2cb6
BaptisteBourlet <baptiste.bourlet@gmail.com>
2019-11-29 16:24:17 +0100

    fct-dare-trustbtn

--- main.js
+++ main.js
@@ -1,64 +1,81 @@
 // mode hot content
 const hotQuestions = {
-    q1 : 'coucou',
-    q2 : "hello",
-    q3 : "francis",
-    q4 : "yoooooo"
+    q1: 'coucou',
+    q2: "hello",
+    q3: "francis",
+    q4: "yoooooo"
 }
 
 const hotGage = {
-    g1 : "bois",
-    g2 : "prie",
-    g3 : "a poil"
+    g1: "bois",
+    g2: "prie",
+    g3: "a poil"
 }
 
 //mode Poseyy content
-
 const fiestaQuestions = {
-    q1 : "yes",
-    q2 : "oui",
-    q3 : "yeaaah"
+    q1: "yes",
+    q2: "oui",
+    q3: "yeaaah"
 }
 
 const fiestaGage = {
-    g1 : "bois",
-    g2 : "aurevoir",
-    g3 : "badaboom"
+    g1: "bois",
+    g2: "aurevoir",
+    g3: "badaboom"
 }
 
-const hotQuestionsList = []
+let hotQuestionsList = [];
 hotQuestionsList.push(Object.values(hotQuestions));
 
-const hotGageList = []
+let hotGageList = [];
 hotGageList.push(Object.values(hotGage));
 
-const fiestaQuestionsList = []
+let fiestaQuestionsList = [];
 fiestaQuestionsList.push(Object.values(fiestaQuestions));
 
-const fiestaGageList = []
+let fiestaGageList = [];
 fiestaGageList.push(Object.values(fiestaGage));
 
+/* ---------------------------- fct randomNumber ---------------------------- */
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 
 (function ($) {
     $(document).ready(function () {
-    
-        let nameList = []
-        $("#submit").click(function(e){
+
+        let nameList = [];
+
+/* --------------------- fct inscription player in form --------------------- */
+
+        $("#submit").click(function (e) {
             var name = $('#enterPlayer').val();
-            nameList.push(name)
+            nameList.push(name);
             $('#enterPlayer').val('');
             $('.playerList').append("<p class='nameListe'>" + name + "</p>");
             e.preventDefault();
         });
-        
 
-        $('#trustBtn').click(function(){
-            
+/* ------------------------------ fct trustBtn ------------------------------ */
+
+        $('#trustBtn').click(function () {
+            $('.listDT').remove();
+            let x = getRandomInt(hotQuestionsList[0].length);
+
+            $('#showDT').append("<p class='listDT'>" + hotQuestionsList[0][x] + "</p>");
+
+        });
+
+/* ------------------------------- fct dareBtn ------------------------------ */
+
+        $('#dareBtn').click(function () {
+            $('.listDT').remove();
+            let x = getRandomInt(hotGageList[0].length);
+
+            $('#showDT').append("<p class='listDT'>" + hotGageList[0][x] + "</p>");
         });
     });
 })(jQuery);
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
